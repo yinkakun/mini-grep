2acdb1f589fae7d38049e4686d3005079716a5fe
yinkakun <yinkakun@gmail.com>
2023-02-19 12:08:29 +0100

    read file to string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    let file_content = fs::read_to_string(file_path).expect("File not found :(");
+
+    println!(
+        "Searching for {} in {} with texts: \n\n{}",
+        query, file_path, file_content
+    );
 }
